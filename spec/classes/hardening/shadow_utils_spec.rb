287db0d4b8ba9db8d85cb65c0436328422184388
Oleksandr Ursu <alexander.ursu@gmail.com>
2024-09-27 10:49:29 +0200

    Added test for UMASK set

--- spec/classes/hardening/shadow_utils_spec.rb
+++ spec/classes/hardening/shadow_utils_spec.rb
@@ -8,6 +8,19 @@ describe 'bsys::hardening::shadow_utils' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile.with_all_deps }
+
+      case os
+      when %r{^centos-7}
+        it {
+          is_expected.to contain_file('/etc/login.defs')
+            .with_content(%r{UMASK 077})
+        }
+      else
+        it {
+          is_expected.to contain_file('/etc/login.defs')
+            .with_content(%r{UMASK 022})
+        }
+      end
     end
   end
 end
