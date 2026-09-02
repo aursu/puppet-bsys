099aea19e198d41db2a1ecd50cba01749fa3bb17
Oleksandr Ursu <alexander.ursu@gmail.com>
2026-09-02 23:23:27 +0200

    Updated PostgreSQL version type to support latest versions as of August 13, 2026

--- spec/type_aliases/bsys_pgversion_spec.rb
+++ spec/type_aliases/bsys_pgversion_spec.rb
@@ -1,9 +1,9 @@
-
 require 'spec_helper'
 
 describe 'Bsys::PGVersion' do
-  it { is_expected.to allow_values('13.20', '13.9', '13.19', '17.4.1') }
+  it { is_expected.to allow_values('18.6', '17.11', '16.15', '15.19', '14.24', '17.4.1') }
 
+  it { is_expected.not_to allow_value('13.23') }
   it { is_expected.not_to allow_value('12.22') }
   it { is_expected.not_to allow_value('17') }
   it { is_expected.not_to allow_value('17.') }
--- types/pgversion.pp
+++ types/pgversion.pp
@@ -1,4 +1,4 @@
-# up to February 16, 2026: PostgreSQL 18.2, 17.8, 16.12, 15.16, 14.21, 13.23 Released!
+# up to August 13, 2026: PostgreSQL 18.6, 17.11, 16.15, 15.19, 14.24 and 19 Beta 3 Released!
 # https://www.postgresql.org/docs/release/
 type Bsys::PGVersion =
-  Pattern[/^(1[3-6]\.[0-9]|13\.(2[0-3]|1[0-9])|14\.(2[0-1]|1[0-9])|15\.1[0-6]|16\.1[0-2]|17\.[0-8]|18\.[0-2])/]
+  Pattern[/^(1[4-7]\.[0-9]|14\.(2[0-4]|1[0-9])|15\.1[0-9]|16\.1[0-5]|17\.1[0-1]|18\.[0-6])/]
